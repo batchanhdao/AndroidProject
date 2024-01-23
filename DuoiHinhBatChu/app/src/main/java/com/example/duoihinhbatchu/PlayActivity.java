6ac6c153d40a100049feb37c0c3bdcf721dbe6c4
batchanhdao <92378282+batchanhdao@users.noreply.github.com>
2024-01-23 10:20:48 +0700

    23/1 time1
    tạo màn chơi và các lựa chọn chơi game đuổi hình bắt chứ

--- DuoiHinhBatChu/app/src/main/java/com/example/duoihinhbatchu/PlayActivity.java
+++ DuoiHinhBatChu/app/src/main/java/com/example/duoihinhbatchu/PlayActivity.java
@@ -3,12 +3,57 @@ package com.example.duoihinhbatchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.duoihinhbatchu.adapter.DapAnAdapter;
+
+import java.util.ArrayList;
 
 public class PlayActivity extends AppCompatActivity {
+    ArrayList<String> arrAnwser;
+    GridView gdvAnwser;
+
+    ArrayList<String> arrSelect;
+    GridView gdvSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        init();
+        anhXa();
+        showAnwser();
+        showSelect();
+    }
+    private void init(){
+        arrAnwser = new ArrayList<>();
+        arrAnwser.add("B");
+        arrAnwser.add("Ba");
+        arrAnwser.add("Be");
+        arrAnwser.add("Bo");
+        arrAnwser.add("Bi");
+        arrAnwser.add("Bu");
+
+        arrSelect = new ArrayList<>();
+        arrSelect.add("Ha");
+        arrSelect.add("Hi");
+        arrSelect.add("Hu");
+        arrSelect.add("He");
+        arrSelect.add("Ho");
+    }
+
+    private  void anhXa(){
+        gdvAnwser = findViewById(R.id.gdvAnwser);
+        gdvSelect = findViewById(R.id.gdvSelect);
+    }
+
+    private void showAnwser(){
+        gdvAnwser.setNumColumns(arrAnwser.size());
+        gdvAnwser.setAdapter(new DapAnAdapter(this, 0, arrAnwser));
+    }
+
+    private void showSelect(){
+        gdvSelect.setNumColumns(arrSelect.size());
+        gdvSelect.setAdapter(new DapAnAdapter(this, 0, arrSelect));
     }
 }
---
+++ DuoiHinhBatChu/app/src/main/java/com/example/duoihinhbatchu/adapter/DapAnAdapter.java
@@ -0,0 +1,36 @@
+package com.example.duoihinhbatchu.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.duoihinhbatchu.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DapAnAdapter extends ArrayAdapter<String> {
+    private Context my_context;
+    private ArrayList<String> arr;
+    public DapAnAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+        this.my_context = context;
+        this.arr = new ArrayList<>(objects);
+    }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) my_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_questions, null);
+        }
+        TextView txvAnwser = convertView.findViewById(R.id.txvAnwser);
+        txvAnwser.setText(this.arr.get(position));
+        return convertView;
+    }
+}
